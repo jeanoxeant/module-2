68fc034ccd2582b9fe8eaa729359622618b59a94
jeanoxeant <jeanrubinoxeant@gmail.com>
2026-05-30 16:54:28 -0400

    Updating my app

--- shop/templatetags/html_formatters.py
+++ shop/templatetags/html_formatters.py
@@ -1,12 +1,12 @@
 from django import template
 
-def currency_euro(value):
-    return f"{value:.2} €"
+def currency_gourde(value):
+    return f"{value:.6} HTG"
 
 def productimage(value):
-    return f"https://raw.github.com/pythonforeveryonetraining/gennaroshop/main/products/{value}"
+      return f"https://raw.github.com/jeanoxeant/oma-distribution/main/products/{value}"
 
 register = template.Library()
 
-register.filter("currency_euro", currency_euro) # name of the filter, function
+register.filter("currency_gourde", currency_gourde) # name of the filter, function
 register.simple_tag(productimage)
